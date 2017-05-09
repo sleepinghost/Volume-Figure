54c963a9bd09c32e21a1483e4cd2f7c8732b40a3
sleepinghost <vlad.mamonov.1997@mail.ru>
2017-05-09 15:58:44 +0700

    Добавлен комментарий в класс Шар

--- Class1/Ball.cs
+++ Class1/Ball.cs
@@ -8,7 +8,7 @@ namespace Model
         double _radius;
 
         /// <summary>
-        /// Установления значения Радиуса
+        /// Установления значения и возврат радиуса Шара
         /// </summary>
         public double Radius
         {
