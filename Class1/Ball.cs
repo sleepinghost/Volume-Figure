73f85b764c8d612a76b9086f4426512da79260d0
sleepinghost <vlad.mamonov.1997@mail.ru>
2017-05-09 17:36:24 +0700

    Добавлены файлы

---
+++ Class1/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Model
+{
+    public  class Ball : IFigure
+    {
+       
+        double _radius;
+
+        /// <summary>
+        /// Установления значения Радиуса
+        /// </summary>
+        public double Radius
+        {
+
+            get { return _radius; }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Значение меньше 0. Введите число больше 0");
+                _radius = value;
+            }
+
+        }
+        /// <summary>
+        /// Начальные координаты, которые хранятся в классе Point
+        /// </summary>
+        public Point StartPoint { get; set; } = new Point();
+
+        /// <summary>
+        /// Рассчитываем объем
+        /// </summary>
+        public double GetVolume()
+        {
+            return ((4.0 / 3.0) * Math.PI * Radius * Radius * Radius);
+        }
+    }
+}
